88959a934866aff59d00858985d903f23eaa09d0
Dhanw <44126779+Dhanw@users.noreply.github.com>
2018-11-06 19:50:56 -0600

    Animal actualizado

--- animal.cpp
+++ animal.cpp
@@ -1,63 +1,30 @@
 #include"Animal.h"
 
 
-Animal::Animal(string ap1,string ap2) :dato(ap1),cod(ap2){}
+Animal::Animal(string ap1,int cd) :dato(ap1),code(cd) {}
 
 bool Animal::operator==(const Animal& p) const {
-	if (this->dato == p.dato && this->cod == p.cod) {
-		return true;
-	}
-	else
-		return false;
+	return this->dato == p.dato && this->code==p.code;
 }
 Animal::Animal() {}
 bool Animal::operator<(const Animal& p) const {
-	/*if (this->cod < p.cod)
-			return true;
-		else
-			return false;*/
-	// forma de una sola linea;
+	return this->code < p.code;
+}
 
-	return this->cod < p.cod;
-
+bool Animal::operator>(const Animal& p) const {
+	return this->code > p.code;
 }
 string Animal::toString() const {
 	stringstream ss;
-	ss << dato << " " /*<< cod << " "*/ << endl;
+	ss << dato << endl;
 	return ss.str();
 }
 
 string Animal::getDato() { return dato; }
-string Animal::getCod() { return cod; }
+int Animal::getCode() { return code; }
 
 ostream& operator<<(ostream& salida, const Animal& p) {
 
 	salida << p.toString();
 	return salida;
 }
-
-//istream & operator>> (istream & Lee, const Animal& p) {
-//	cout << "\n\nIngrese nombre de la Persona:";
-//	// ingresar el dato principal
-//	Lee >> p.getDato;
-//	cout << "\n\nIngrese anio de nacimiento:";
-//	// ingresar el cod que parece quw usare más tarde;
-//	Lee >> p.getCod;
-//	return Lee;
-//}
-
-//ostream & operator +(ostream &escribe, Animal& p) {
-//	escribe << p.getDato << " " << p.getCod << endl;
-//	return escribe;
-//
-//}
-
-//Animal::Animal() {}
-//bool Animal::operator<(const Animal& p) const {
-//	/*if (this->dato < p.dato)
-//	return true;
-//	else*/ if (this->cod < p.cod)
-//		return true;
-//	else
-//		return false;
-//}
